94e9b9d41133756377efd23f067391733d058dea
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-09 16:06:28 +0900

    url.py 주석추가

--- articles/urls.py
+++ articles/urls.py
@@ -4,16 +4,16 @@ from django.urls import path
 
 urlpatterns = [
 
-    path('',views.ArticlesView.as_view(), name = 'article'),
-    path('<int:article_id>',views.ArticlesView.as_view(), name = 'article'),
+    path('',views.ArticlesView.as_view(), name = 'article'), # /articles/
+    path('<int:article_id>/',views.ArticlesView.as_view(), name = 'article'), # /articles/<int:article_id>/
     
-    path('<int:article_id>/hearts/',views.HeartsView.as_view(), name='Hearts_View'),  # 좋아요 기능
-    path('<int:user_id>/hearts/',views.HeartsView.as_view(), name='User_Hearts_View'), # 좋아요 한 게시글
+    path('<int:article_id>/hearts/',views.HeartsView.as_view(), name='Hearts_View'),  # 좋아요 기능 /articles/<int:article_id>/hearts/
+    path('<int:user_id>/hearts/',views.HeartsView.as_view(), name='User_Hearts_View'), # 좋아요 한 게시글 /articles/<int:user_id>/hearts/
   
-    path('<int:article_id>/bookmarks/',views.BookMarksView.as_view(), name='bookmarks_View'),  # 북마크 기능
-    path('<int:user_id>/bookmarks/',views.BookMarksView.as_view(), name='bookmarks_View'),  # 북마크 한 게시글
+    path('<int:article_id>/bookmarks/',views.BookMarksView.as_view(), name='bookmarks_View'),  # 북마크 기능 /articles/<int:article_id>/bookmarks/
+    path('<int:user_id>/bookmarks/',views.BookMarksView.as_view(), name='bookmarks_View'),  # 북마크 한 게시글 /articles/<int:user_id>/bookmarks/
   
-    path('<int:article_id>/comment/', views.CommentsView.as_view(), name="comment_view"),
-    path('<int:article_id>/comment/<int:comment_id>/',views.CommentsdetailView.as_view(), name="comment_detail_view"),
+    path('<int:article_id>/comment/', views.CommentsView.as_view(), name="comment_view"), # /articles/<int:article_id>/comment/
+    path('<int:article_id>/comment/<int:comment_id>/',views.CommentsdetailView.as_view(), name="comment_detail_view"), # /articles/<int:article_id>/comment/<int:comment_id>/
 
 ]
