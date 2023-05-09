595d0f8c19f5c88548fdadaa5277f6a861802490
Kim SungWoo <gokorea1214@kakao.com>
2023-05-09 14:48:41 +0900

    urls.py 에 로그인 회원가입 추가

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,6 @@ from users import views
 urlpatterns = [
     path('profile/<int:user_id>', views.ProfileView.as_view(), name="profile_view"),
     path('follow/<int:user_id>/', views.FollowView.as_view(), name='follow_view'),
+    path('signup/', views.SignupView.as_view(), name='sign_up_view'),
+    path('login/', views.CustomTokenObtainPairView.as_view(), name='login_view'),
 ]
