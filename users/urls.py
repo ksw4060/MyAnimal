18cfa63908c82dcb236aa6d38ffdcb30c3720a05
Kim SungWoo <gokorea1214@kakao.com>
2023-05-10 11:34:01 +0900

    urls.py 띄워쓰기

--- users/urls.py
+++ users/urls.py
@@ -6,9 +6,14 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('signup/', views.SignupView.as_view(), name='sign_up_view'), # /users/signup/
-    path('login/', views.CustomTokenObtainPairView.as_view(), name='login_view'), # /users/login/
-    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"), # /users/refresh/
+    path('signup/', views.SignupView.as_view(),
+         name='sign_up_view'),  # /users/signup/
+    path('login/', views.CustomTokenObtainPairView.as_view(),
+         name='login_view'),  # /users/login/
+    path("refresh/", TokenRefreshView.as_view(),
+         name="token_refresh"),  # /users/refresh/
+
     path('profile/<int:user_id>/', views.ProfileView.as_view(), name="profile_view"),
     path('follow/<int:user_id>/', views.FollowView.as_view(), name='follow_view'),
+
 ]
