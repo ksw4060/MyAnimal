7915238c8d0d1643c5f21a7be4e37d6376e1860c
Lee Juneyoung <wnsdud318@gmail.com>
2023-05-12 16:27:51 +0900

    미디어, 스태틱 설정 수정

--- MyAnimal/settings.py
+++ MyAnimal/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_ROOT = path.join(BASE_DIR, 'static')
+STATIC_ROOT = path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
-# MEDIA_ROOT = path.join(BASE_DIR, 'media')
-# MEDIA_URL = 'media/'
+MEDIA_ROOT = path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
