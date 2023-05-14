c0004511af64491f057411bb2b7463e73f7dfba7
김성우 <127410562+ksw406020230309@users.noreply.github.com>
2023-05-14 21:50:46 +0900

    Update views.py

--- articles/views.py
+++ articles/views.py
@@ -125,19 +125,16 @@ class ArticlesDetailView(APIView):  # /articles/id/
 
 
 # ====================== 게시글 좋아요 ================================
+
 class HeartsView(APIView):
     def post(self, request, article_id):
         article = get_object_or_404(Articles, id=article_id)
-        if request.user != article.user:
-            if request.user in article.hearts.all():
-                article.hearts.remove(request.user)
-                return Response('좋아요 취소', status=status.HTTP_200_OK)
-            else:
-                article.hearts.add(request.user)
-                return Response('좋아요', status=status.HTTP_200_OK)
+        if request.user in article.hearts.all():
+            article.hearts.remove(request.user)
+            return Response('좋아요 취소', status=status.HTTP_200_OK)
         else:
-            return Response('본인의 글에는 좋아요 할 수 없습니다.')
-
+            article.hearts.add(request.user)
+            return Response('좋아요', status=status.HTTP_200_OK)
 
 # ====================== 좋아요 한 게시글 보기 ================================
 
